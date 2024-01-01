90b58e5f4905a475ae469dfacce45dd520e9574c
Duy Lam <duylamjunpio@gmail.com>
2024-01-01 19:43:11 +0700

    project initialization

--- QLBH/src/App.vue
+++ QLBH/src/App.vue
@@ -1,9 +1,26 @@
-<script setup>
-</script>
-
 <template>
-  <h1>Khởi tạo dự án</h1>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Duy Lâm đẹp trai vãi ò" />
 </template>
 
-<style scoped>
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
